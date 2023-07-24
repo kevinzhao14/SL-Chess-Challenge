debf6df4dd97e416707546893322dea666a9a5b9
Kevin Zhao <kevin@randume.org>
2023-07-24 01:11:27 -0400

    Kinda fix Alpha-Alpha pruning

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,5 +1,7 @@
 ﻿using ChessChallenge.API;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class MyBot : IChessBot
 {
@@ -11,45 +13,116 @@ public class MyBot : IChessBot
         {0x80797c847a76746c, 0x727d848183867f80, 0x7b817c7f7e818582, 0x7c767c7c7f7e817f, 0x75757a7a80877f80, 0x7b7983838b969296, 0x77717e807a968b95, 0x75808b8597919192},
         {0x7a8e856b83758985, 0x80837d676f7a8483, 0x7b7b776e6f747a75, 0x6d8075716e6f736c, 0x79787b7574767b72, 0x7c89817a78828977, 0x8b80787d7d7e7175, 0x6789867a6a738185}
     };
-    long[] pieceValues = {0, 32, 98, 107, 180, 304, 0};
+    int[] pieceValues = {0, 32, 98, 107, 180, 304, 0};
+
+    int nodes = 0;
 
     public Move Think(Board board, Timer timer)
     {
-        // Console.WriteLine("-------------------");
-        int depth = 1;
+        Console.WriteLine("-------------------");
+        nodes = 0;
+        int depth = 4;
+        Move best = BestMove(board, depth, -999999999999, 99999999999, depth % 2 != 0, new Move[depth]).Item1;
+        Console.WriteLine("Nodes checked: " + nodes);
 
-        Move[] moves = board.GetLegalMoves();
+        return best;
 
-        Move bestMove = moves[0];
-        long bestScore = -999999999;
-
-        foreach (Move move in moves) {
-            board.MakeMove(move);
-            
-            long score = eval(board);
-
-            // Console.WriteLine("Move " + score + " " + move);
-
-            if (score > bestScore) {
-                bestMove = move;
-                bestScore = score;
-            }
-
-            board.UndoMove(move);
-        }
-
-        return bestMove;
+        // return bestMove;
     }
 
-    long eval(Board board) {
+    long Eval(Board board) {
         long moveScore = 0;
         foreach (PieceList list in board.GetAllPieceLists()) {
             foreach(Piece piece in list) {
-                moveScore += pieceValues[(int) piece.PieceType] * (piece.IsWhite == !board.IsWhiteToMove ? 1 : -1);
                 Square sq = piece.Square;
-                moveScore += ((long) pst[(int) piece.PieceType - 1, piece.IsWhite ? sq.Rank : 7 - sq.Rank] >> (56 - 8 * sq.File) & 0xFF) - 128;
+                long score = pieceValues[(int) piece.PieceType];
+                score += ((long) pst[(int) piece.PieceType - 1, piece.IsWhite ? sq.Rank : 7 - sq.Rank] >> (56 - 8 * sq.File) & 0xFF) - 128;
+                moveScore += score * (piece.IsWhite == board.IsWhiteToMove ? 1 : -1);
             }
         }
         return moveScore;
+    }
+
+    (Move, long, Move[]) BestMove(Board board, int depth, long alpha, long beta, bool oddDepth, Move[] line) {
+        Move bestMove = new Move();
+        long bestScore = -999999999;
+        Move[] bestLine = {};
+
+        // Move[] moves = board.GetLegalMoves();
+
+        List<(Move, int)> moves = new List<(Move, int)>();
+
+        foreach (Move move in board.GetLegalMoves()) {
+            int score = 0;
+            if (move.IsCapture) {
+                score += pieceValues[(int) move.CapturePieceType] - pieceValues[(int) move.MovePieceType];
+            }
+            // if (move.IsPromotion) {
+            //     score += pieceValues[(int) move.PromotionPieceType] / 8;
+            // }
+            // if (move.IsCastles) {
+            //     score += 8;
+            // }
+
+            moves.Add((move, score));
+        }
+
+        // if (depth == 4) Console.WriteLine("moves " + string.Join(", ", moves));
+
+        moves.Sort((a, b) => b.Item2 - a.Item2);
+        
+
+        // foreach (var move in moves) {
+        foreach (var movedata in moves) {
+            nodes++;
+
+            Move move = movedata.Item1;
+            board.MakeMove(move);
+            Move[] moveLine = (Move[]) line.Clone();
+            moveLine[moveLine.Length - depth] = move;
+
+            long score = 0;
+
+            if (board.IsInCheckmate()) {
+                board.UndoMove(move);
+                return (move, 100000, moveLine);
+            } else if (board.IsDraw()) {
+                score = 0;
+            } else if (depth > 1) {
+                var (m, s, l) = BestMove(board, depth - 1, -beta, -alpha, oddDepth, moveLine);
+                score = -s;
+                moveLine = l;
+
+                if (score == 100000) {
+                    board.UndoMove(move);
+                    return (move, 100000, moveLine);
+                }
+            } else {
+                score = Eval(board) * (oddDepth ? 1 : -1);
+            }
+
+            board.UndoMove(move);
+
+            // if (depth == 4) {
+            //     Console.WriteLine("m " + score + " " + string.Join(", ", moveLine));
+            // }
+
+            if (score > beta) {
+                return (move, score, moveLine);
+            }
+            
+            if (score > bestScore) {
+                bestMove = move;
+                bestScore = score;
+                bestLine = moveLine;
+            }
+
+            if (score > alpha) {
+                alpha = score;
+            }
+
+        }
+
+        return (bestMove, bestScore, bestLine);
     }
 }
